4d38c632a2faf81fd1e3b925ac7c88deb3473d8d
Alejandro Ramirez <alejandro.ramirez@local.ch>
2015-01-16 14:20:59 +0100

    reduce clutter

--- lib/magnum/cli/jenkins.rb
+++ lib/magnum/cli/jenkins.rb
@@ -13,25 +13,30 @@ module Magnum
     desc 'create', 'Create jenkins CI job for this module'
     def create()
       generate_config
+
       f = File.read('config.xml')
-      user = ask("jenkins username:")
-      pass = ask("jenkins pass:")
+      user = options.has_key?('ldap_user') ? options[:ldap_user] : ask("jenkins username:")
+      pass = options.hast_key?('ldap_pass') ? options[:ldap_pass] : ask("jenkins pass:", :echo => false)
       job_name = self.project_url.split(/\//).last
       puts("https://jenkins.intra.local.ch/createItem?name=#{job_name}")
       jobs = RestClient::Resource.new("https://jenkins.intra.local.ch/createItem?name=#{job_name}",
-                                   user,
-                                   pass)
-      jobs.post(f, {:content_type => 'application/xml'})
+      user,
+      pass)
+      jobs.post(f, {:content_type => 'application/xml'}){|response, request, result, &block|
+        case response.code
+        when 200
+          puts("Successfuly created job!")
+          response
+        else
+          puts("Could not create jenkins job!")
+          response.return!(request, result, &block)
+        end
+      }
     end
 
     private
     def generate_config
-      begin
-        g = Git.open('./')
-      rescue ArgumentError
-        puts('There is no .git repo here!!')
-        abort
-      end
+      g = Git.open('./')
       origin_index = g.remotes.index{|b| b.name == 'origin'}
       origin = g.remotes[origin_index]
       self.git_url = origin.url
@@ -40,10 +45,6 @@ module Magnum
       template 'ci/config.xml.erb', target.join('config.xml')
     end
 
-    private
-    def clean_config
-
-    end
 
     def self.banner(task, namespace = false, subcommand = true)
       "#{basename} #{task.formatted_usage(self, namespace, subcommand).split(':').join(' ')}"
@@ -57,6 +58,16 @@ module Magnum
     def self.source_root
       Magnum.root.join('generator_files')
     end
+
+    private
+    def options
+      original_options = super
+      rcfile = File.expand_path('~/.magnumrc')
+      return original_options unless File.exists?(rcfile)
+      defaults = ::YAML::load_file(rcfile) || {}
+      Thor::CoreExt::HashWithIndifferentAccess.new(defaults.merge(original_options))
+    end
+
 
   end
 end
